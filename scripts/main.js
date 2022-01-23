373cc767498f6c885caa7a7784b00cfd7c9e1e97
Honma Ryota <m95.honma.ryota@gmail.com>
2022-01-23 23:40:55 +0900

    SP menu test

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,30 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const els = document.querySelectorAll(".animate-title");
-  const cb = function (entries, observer) {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        const ta = new TextAnimation(entry.target);
-        ta.animate();
-        observer.unobserve(entry.target);
-      }
-    });
-  };
-  const options = {
-    root: null,
-    rootMargin: "0px",
-    threshold: 0,
-  };
-  const io = new IntersectionObserver(cb, options);
-  els.forEach((el) => {
-    io.observe(el);
-  });
-});
+class MobileMenu {
+  constructor() {
+    this.DOM = {};
+    this.DOM.btn = document.querySelector(".mobile-menu__btn");
+    this.DOM.cover = document.querySelector(".mobile-menu__cover");
+    this.DOM.container = document.querySelector("#global-container");
+    this.eventType = this._getEventType();
+    this._addEvent();
+  }
+
+  _getEventType() {
+    const isTouchCapable =
+      "ontouchstart" in window ||
+      (window.DocumentTouch && document instanceof window.DocumentTouch) ||
+      navigator.maxTouchPoints > 0 ||
+      window.navigator.msMaxTouchPoints > 0;
+
+    return isTouchCapable ? "touchstart" : "click";
+  }
+
+  _toggle() {
+    this.DOM.container.classList.toggle("menu-open");
+  }
+  _addEvent() {
+    this.DOM.btn.addEventListener(this.eventType, this._toggle.bind(this));
+    this.DOM.cover.addEventListener(this.eventType, this._toggle.bind(this));
+  }
+}
+
+new MobileMenu();
